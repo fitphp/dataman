036b3b208145a82550f69434d89bbdfbd1554339
Bincent <hongbin.hsu@bincent.com>
2021-01-08 22:55:52 +0800

    修改首页

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Admin\Controllers;
 
-use App\Admin\Metrics\Examples;
 use App\Http\Controllers\Controller;
 use Dcat\Admin\Http\Controllers\Dashboard;
 use Dcat\Admin\Layout\Column;
@@ -15,22 +14,41 @@ class HomeController extends Controller
     {
         return $content
             ->header('控制台')
-            ->description('用于展示当前系统中的统计数据、统计报表及重要实时数据')
+            ->description('当前系统基本信息')
             ->body(function (Row $row) {
-                $row->column(6, function (Column $column) {
-                    $column->row(Dashboard::title());
-                    $column->row(new Examples\Tickets());
+                $row->column(4, function (Column $column) {
+                    $column->row($this->dependencies());
                 });
 
-                $row->column(6, function (Column $column) {
-                    $column->row(function (Row $row) {
-                        $row->column(6, new Examples\NewUsers());
-                        $row->column(6, new Examples\NewDevices());
-                    });
-
-                    $column->row(new Examples\Sessions());
-                    $column->row(new Examples\ProductOrders());
+                $row->column(8, function (Column $column) {
+                    $column->row($this->environment());
                 });
             });
+    }
+
+    public function environment()
+    {
+        $envs = [
+            ['name' => 'PHP版本',   'value' => 'PHP/'.PHP_VERSION],
+            ['name' => '框架版本',   'value' => 'Laravel '.app()->version()],
+            ['name' => '操作系统',   'value' => php_uname()],
+            ['name' => '软件配置',   'value' => $_SERVER["SERVER_SOFTWARE"]],
+            ['name' => '缓存驱动',   'value' => config('cache.default')],
+            ['name' => '会话驱动',   'value' => config('session.driver')],
+            ['name' => '队列驱动',   'value' => config('queue.default')],
+            ['name' => '系统时区',   'value' => config('app.timezone')],
+            ['name' => '区域设置',   'value' => config('app.locale')],
+        ];
+
+        return view('admin::dashboard.environment', compact('envs'));
+    }
+
+    public function dependencies()
+    {
+        $json = file_get_contents(base_path('composer.json'));
+
+        $dependencies = json_decode($json, true)['require'];
+
+        return view('admin::dashboard.dependencies', compact('dependencies'));
     }
 }
