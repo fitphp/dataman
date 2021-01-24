163c0ec3b877aa48d0276cf455e8db0f5214fd9a
Boolean <hongbin.hsu@qq.com>
2021-01-24 22:43:40 +0800

    style :  菜单语言

--- resources/lang/zh_CN/menu.php
+++ resources/lang/zh_CN/menu.php
@@ -2,16 +2,18 @@
 
 return [
     'titles' => [
-        'index'         => '主页',
-        'admin'         => '系统',
-        'users'         => '管理员',
-        'roles'         => '角色',
-        'permission'    => '权限',
-        'menu'          => '菜单',
-        'operation_log' => '操作日志',
-        'helpers'       => '开发工具',
-        'extensions'    => '扩展',
-        'scaffold'      => '代码生成器',
-        'icons'         => '图标',
+        'index'                 => '主页',
+        'admin'                 => '系统',
+        'users'                 => '管理员',
+        'roles'                 => '角色',
+        'permission'            => '权限',
+        'menu'                  => '菜单',
+        'operation_log'         => '操作日志',
+        'helpers'               => '开发工具',
+        'extensions'            => '扩展',
+        'scaffold'              => '代码生成器',
+        'icons'                 => '图标',
+        'china region'          => '中国地区',
+        'category manager'      => '分类'
     ],
 ];
