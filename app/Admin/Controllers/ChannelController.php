e2e15f230381ef99fb073e6b5ec40192cebe2be9
Boolean <p_hongbinxu@tencent.com>
2021-02-19 21:54:31 +0800

    fix :#3892I 栏目管理编辑保存时报 405 Method Not Allowed

--- app/Admin/Controllers/ChannelController.php
+++ app/Admin/Controllers/ChannelController.php
@@ -7,46 +7,11 @@ use App\Models\Platform as PlatformModels;
 use App\Admin\Repositories\Channel;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid;
-use Dcat\Admin\Layout\Content;
-use Dcat\Admin\Layout\Row;
 use Dcat\Admin\Show;
 use Dcat\Admin\Http\Controllers\AdminController;
-use Dcat\Admin\Tree;
 
 class ChannelController extends AdminController
 {
-//    public function index(Content $content)
-//    {
-//        return $content->title(trans('channel.title'))
-//            ->description(trans('admin.list'))
-//            ->body(function (Row $row) {
-//                $row->column(6, $this->treeView()->render());
-//                $row->column(6, $this->form());
-//            });
-//    }
-//
-//    protected function treeView()
-//    {
-//        $tree = new Tree(new Channel(['platform']));
-//        $tree->disableCreateButton();
-//        $tree->disableQuickCreateButton();
-//        $tree->branch(function ($branch) {
-//            $image = null;
-//            if (!empty($branch['image'])) {
-//                $src = config('admin.upload.host') . '/' . $branch['image'] ;
-//                $image = "<img src='$src' style='max-width:30px;max-height:30px' class='img'/>";
-//            }
-//
-//            if (empty($branch['title'])) {
-//                return "{$branch['id']}  - {$branch['platform']['name']} - {$image} {$branch['title']}";
-//            } else {
-//                return "{$branch['id']}  - {$branch['platform']['name']} - {$image} {$branch['title']}【{$branch['name']}】";
-//            }
-//        });
-//
-//        return $tree;
-//    }
-
     /**
      * Make a grid builder.
      *
@@ -102,8 +67,7 @@ class ChannelController extends AdminController
     protected function form()
     {
         return Form::make(new Channel(), function (Form $form) {
-            $form->action(admin_url('channel'));
-
+            $form->display('id');
             $form->select('platform_id', trans('channel.fields.platform_id'))
                 ->options(PlatformModels::all()->pluck('name','id'))
                 ->default(0)->required();
